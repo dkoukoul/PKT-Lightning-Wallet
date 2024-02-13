0676eae0e3c8d0483cb2e3b23b1e418c97bf11ff
Caleb James DeLisle <cjd@cjdns.fr>
2024-02-13 15:26:36 +0100

    Try renaming the executables for mac and linux

--- contrib/macos/build.sh
+++ contrib/macos/build.sh
@@ -4,4 +4,4 @@
 # This script should be run from the project root
 # e.g. ./contrib/gem/build.sh
 #
-fpm --prefix /usr/local -n pktd-mac -s dir -t osxpkg -v "$(./bin/pld --version | sed 's/.* version //' | tr -d '\n')" ./bin
+fpm --prefix /usr/local -n PKT-Lightning-Wallet-mac -s dir -t osxpkg -v "$(./bin/pld --version | sed 's/.* version //' | tr -d '\n')" ./bin
